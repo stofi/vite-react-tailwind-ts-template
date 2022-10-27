0b0b9efe5e04e151c3b170d6979b581c97616436
Krystof Rainisch <Krystof.Rainisch@hey.com>
2022-10-27 21:42:17 +0200

    Init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <div className='App'>
+      <HelloWorld>
+        <h1>Hello from React with TypeScript and Tailwind!</h1>
+      </HelloWorld>
+    </div>
+  )
+}
+
+export default App
